56575cd78ab5e4b30561675222010af7c9240217
Deniss <deniss@altermail.co>
2023-05-09 16:02:41 +0300

    BBLOCK-224 session fix

--- src/common/HttpRequest.js
+++ src/common/HttpRequest.js
@@ -3,7 +3,6 @@ import jsonBigint from "json-bigint";
 import UserSessionDto from "@/common/dto/UserSessionDto.js";
 import { HttpResponse } from "@/common/HttpResponse.js";
 import store from "@/state/store";
-import router from "../router/router-config";
 import Toast from "@/mixins/Toast.js";
 
 export class HttpRequest {
@@ -36,12 +35,11 @@ export class HttpRequest {
       request
     );
 
-    if (response.status !== 200) {
+    if (response.status === 401) {
       // user is logged out
       this.session = null;
       store.commit("Auth/setUser", null);
       store.commit("Project/setProject", null);
-      await router.push("/");
       Toast.methods.Toast("Session timeout", "", "warning");
     }
 
