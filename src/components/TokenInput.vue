7f99597dbcdba224de901f2fcdd30056217783a6
Imants Kaupužs <imants@alter.network>
2023-05-10 09:36:18 +0300

    BBLOCK-137 Updated token input to act like decimal input but always output correct integer value for the given token type.

--- src/components/TokenInput.vue
+++ src/components/TokenInput.vue
@@ -1,15 +1,14 @@
 <template>
-  <el-input v-model="displayValue" placeholder="Amount" />
-  <sub v-if="contract">
-    {{ modelValue }}
-  </sub>
+  <el-input v-model="inputValue" placeholder="Amount" />
 </template>
+
 <script>
 import Contract from "@/state/models/Contract.js";
 import { Formatter } from "@/common/Formatter.js";
+import DebugWrapper from "@/components/DebugWrapper.vue";
 
 export default {
-  components: {},
+  components: { DebugWrapper },
   props: {
     modelValue: {
       type: [String, BigInt],
@@ -17,40 +16,101 @@ export default {
     },
     contract: {
       type: Contract,
-      default: null,
+      default() {
+        const contract = new Contract();
+
+        contract.decimalSpaces = 6;
+        contract.denom = "";
+
+        return contract;
+      },
     },
   },
   emits: ["update:modelValue"],
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
   computed: {
-    displayValue: {
-      get: function () {
-        if (this.contract == null) {
-          return "";
-        }
-        return this.Formatter.token(
-          this.modelValue,
-          this.contract,
-          this.contract.decimalSpaces
-        );
-      },
-      set: function (modifiedValue) {
-        if (this.contract == null) {
-          return;
-        }
-        let newValue = parseFloat(modifiedValue.replace(/[^\d.]/g, ""));
-        if (isNaN(newValue)) {
-          newValue = 0;
-        }
-
-        let decimalSpaces = Math.pow(
-          10,
-          this.contract.decimalSpaces
-        ).toString();
-        let value = BigInt(newValue.toString() * decimalSpaces);
-        this.$emit("update:modelValue", value);
-      },
+    decimalSpaces() {
+      return this.contract?.decimalSpaces || 6;
     },
-    Formatter: () => Formatter,
+    value() {
+      if (this.modelValue === "" || isNaN(this.modelValue)) {
+        return null;
+      }
+
+      return BigInt(this.modelValue);
+    },
+  },
+  watch: {
+    inputValue: "parseUserInput",
+    value: "setInputValue",
+    contract() {
+      this.parseUserInput(this.inputValue, "");
+    },
+  },
+  methods: {
+    parseUserInput(newValue, oldValue) {
+      if (isNaN(newValue)) {
+        this.inputValue = isNaN(oldValue) ? "" : oldValue;
+
+        return;
+      }
+
+      if (newValue === "") {
+        this.$emit("update:modelValue", "");
+
+        return;
+      }
+
+      const [wholes, decimals] = newValue.split(".");
+
+      if (decimals !== undefined && decimals.length > this.decimalSpaces) {
+        this.inputValue = oldValue;
+
+        return;
+      }
+
+      this.$emit(
+        "update:modelValue",
+        wholes + (decimals || "").padEnd(this.decimalSpaces, "0")
+      );
+    },
+    setInputValue() {
+      if (this.value === null) {
+        this.inputValue = "";
+
+        return;
+      }
+
+      if (this.value === 0) {
+        this.inputValue = "0";
+
+        return;
+      }
+
+      const numberString = Formatter.bigIntToPrecision(
+        this.value,
+        this.decimalSpaces,
+        this.decimalSpaces
+      );
+
+      if (this.inputValue && numberString.includes(this.inputValue)) {
+        return;
+      }
+
+      let [wholes, decimals] = numberString.split(".");
+
+      decimals = decimals.replace(/0+$/, "");
+
+      if (decimals.length === 0) {
+        this.inputValue = wholes;
+      } else {
+        this.inputValue = `${wholes}.${decimals}`;
+      }
+    },
   },
 };
 </script>
