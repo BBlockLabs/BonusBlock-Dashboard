a2a0bbfe450ecd5f4c7164338583838f22bdfa66
Slava <slava@altermail.co>
2023-05-26 13:14:58 +0300

    BBLOCK-302: Update project dashboard charts to meet the new specification

--- src/main.js
+++ src/main.js
@@ -16,6 +16,14 @@ import VueApexCharts from "vue3-apexcharts";
 
 import "@/design/index.scss";
 
+// ISO-8601, Europe
+moment.updateLocale("en", {
+  week: {
+    dow: 1, // First day of week is Monday
+    doy: 4, // First week of year must contain 4 January (7 + 1 - 4)
+  },
+});
+
 const app = createApp({
   extends: App,
 });
--- src/views/CampaignAnalytics.vue
+++ src/views/CampaignAnalytics.vue
@@ -22,41 +22,56 @@
             </el-row>
             <el-row class="mt-1" align="middle">
               <el-col :span="-1">
-                <b>Product: {{ campaign ? campaign.name : "" }} Categories: </b>
+                <b>Product:</b> {{ campaign ? campaign.name : "" }}
+              </el-col>
+              <el-col :span="-1" class="ml-3">
+                <b>Categories:</b>
               </el-col>
               <el-col v-if="campaign" :span="-1">
                 <el-tag
-                  v-for="category in categories"
-                  :key="category.id"
+                  v-for="tag in campaign.tags"
+                  :key="tag"
                   class="mx-1"
                   round
                 >
-                  {{ category.name }}
+                  {{ tag }}
                 </el-tag>
               </el-col>
             </el-row>
-
-            <el-row align="middle">
-              <el-col class="mt-1" :span="-1">
-                <el-tabs v-model="interactionsRange">
-                  <el-tab-pane label="All Time" name="all"></el-tab-pane>
-                  <el-tab-pane label="Month" name="month"></el-tab-pane>
-                  <el-tab-pane label="Week" name="week"></el-tab-pane>
-                  <el-tab-pane label="Today" name="today"></el-tab-pane>
-                </el-tabs>
-              </el-col>
-              <!--              <el-col :span="-1">
-                              <el-button class="ml-4" type="default">Set custom period</el-button>
-                            </el-col>-->
-            </el-row>
             <el-row>
               <el-col
-                v-loading="chartLoading[interactionsRange]"
                 :md="12"
                 style="min-height: 20em"
               >
+                <h3>Interactions</h3>
+                <el-row align="middle">
+                  <el-col class="mt-1" :span="-1">
+                    <el-tabs v-model="interactionsRange">
+                      <el-tab-pane label="Year" name="year"></el-tab-pane>
+                      <el-tab-pane label="Month" name="month"></el-tab-pane>
+                      <el-tab-pane label="Week" name="week"></el-tab-pane>
+                      <el-tab-pane label="Day" name="day"></el-tab-pane>
+                    </el-tabs>
+                  </el-col>
+                  <el-col :span="-1" class="ml-auto">
+                    <el-row align="middle" class="chart-navigation">
+                      <svg-chevron-left
+                        class="prevent-select"
+                        :class="{ disabled: !hasPrevRange }"
+                        @click="jumpBy(1)"
+                      />
+                      <strong>{{ currentRangeName }}</strong>
+                      <svg-chevron-right
+                        class="prevent-select"
+                        :class="{ disabled: !hasNextRange }"
+                        @click="jumpBy(-1)"
+                      />
+                    </el-row>
+                  </el-col>
+                </el-row>
                 <apexchart
                   v-if="interactionsSeries[0].data.length > 0"
+                  v-loading="chartLoading[interactionsRange + chartOffset]"
                   ref="interactionsChart"
                   style="width: 100%; height: 100%"
                   height="100%"
@@ -66,7 +81,7 @@
                 ></apexchart>
                 <div v-else>No data yet</div>
               </el-col>
-              <el-col v-loading="analyticsLoading" :md="12" class="pl-3">
+              <el-col v-loading="analyticsLoading" :md="12" class=" pl-3 d-flex flex-column justify-content-center">
                 <el-card class="analytics-card yellow" shadow="never">
                   <el-row class="d-flex mt-3 mb-4">
                     <el-col
@@ -177,14 +192,19 @@
 import { CampaignAnalyticsDto } from "@/common/dto/CampaignAnalyticsDto.js";
 import moment from "moment";
 import VueApexCharts from "vue3-apexcharts";
+import SvgChevronLeft from "@/assets/icons/nav-arrow-left.svg";
+import SvgChevronRight from "@/assets/icons/nav-arrow-right.svg";
 
 export default {
   components: {
     apexchart: VueApexCharts,
+    SvgChevronLeft,
+    SvgChevronRight,
   },
   data() {
     return {
-      interactionsRange: "today",
+      interactionsRange: "day",
+      chartOffset: 0,
       chartLoading: {},
       analyticsLoading: false,
       analytics: new CampaignAnalyticsDto(),
@@ -193,18 +213,29 @@ export default {
       interactionsCache: {},
       interactionsOptions: {
         colors: ["#EDC16C"],
-
+        chart: {
+          animations: {
+            speed: 400,
+          },
+          toolbar: {
+            show: false,
+          },
+        },
         theme: {
           mode: "light",
-        },
-        title: {
-          text: "Interactions",
-          align: "left",
         },
         xaxis: {
           categories: [""],
           labels: {
             hideOverlappingLabels: true,
+          },
+        },
+        tooltip: {
+          x: {
+            formatter: this.tooltipXFormatter,
+          },
+          y: {
+            formatter: this.tooltipYFormatter,
           },
         },
         dataLabels: {
@@ -285,15 +316,51 @@ export default {
     campaign() {
       return this.$store.getters["Campaign/getCampaign"](this.campaignId);
     },
-    categories() {
-      return this.campaign.categories ? this.campaign.categories.map(
-        this.$store.getters["Category/getCategory"]
-      ) : [];
+    currentRangeStart() {
+      return moment()
+        .subtract(this.chartOffset, this.interactionsRange)
+        .startOf(this.interactionsRange);
+    },
+    currentRangeEnd() {
+      return moment()
+        .subtract(this.chartOffset, this.interactionsRange)
+        .endOf(this.interactionsRange);
+    },
+    currentRangeName() {
+      let rangeEnd = this.currentRangeStart;
+      switch (this.interactionsRange) {
+        case "day":
+          return rangeEnd.format("MMMM Do");
+        case "week":
+          return this.nth(this.weekOfMonth(rangeEnd)) + " week of " + rangeEnd.format("MMMM");
+        case "month":
+          return rangeEnd.format("MMMM");
+        case "year":
+          return rangeEnd.format("YYYY");
+      }
+      return "";
+    },
+    hasPrevRange() {
+      //let startDate = ;
+      //if (!startDate) {
+      //  return true;
+      //}
+      //return this.currentRangeStart.isAfter(startDate);
+      return true;
+    },
+    hasNextRange() {
+      return this.chartOffset > 0;
     },
   },
   watch: {
     interactionsRange(newValue, oldValue) {
       if (newValue !== oldValue && newValue !== "") {
+        this.chartOffset = 0;
+        this.fetchCampaignInteractions();
+      }
+    },
+    chartOffset(newValue, oldValue) {
+      if (newValue !== oldValue) {
         this.fetchCampaignInteractions();
       }
     },
@@ -319,33 +386,21 @@ export default {
       }
       this.analyticsLoading = false;
     },
-    parseChartRange(range) {
-      let from = null;
-      if (range !== "all") {
-        let periodFrom = moment().startOf("day").utc();
-        switch (range) {
-          case "today":
-            break;
-          case "week":
-            periodFrom.subtract(7, "days");
-            break;
-          case "month":
-            periodFrom.subtract(32, "days");
-            break;
-          case "year":
-            periodFrom.subtract(1, "year");
-            break;
-        }
-        from = periodFrom.unix();
-      }
-      return {
-        from: from,
-        to: null,
-      };
-    },
     async fetchCampaignInteractions() {
+      const truncateSettings = {
+        day: "hours",
+        week: "days",
+        month: "days",
+        year: "months",
+      };
       let queriedRange = this.interactionsRange;
-      let query = this.parseChartRange(queriedRange);
+      let queriedOffset = this.chartOffset;
+      let query = {
+        from: this.currentRangeStart.unix(),
+        to: this.currentRangeEnd.unix(),
+        truncateTo: truncateSettings[this.interactionsRange],
+        timeZoneOffset: new Date().getTimezoneOffset() * -1,
+      };
       let cacheKey = query.from + "-" + query.to;
       if (this.interactionsCache[cacheKey]) {
         this.updateChart(this.interactionsCache[cacheKey].data);
@@ -356,10 +411,10 @@ export default {
           return;
         }
       }
-      if (this.chartLoading[queriedRange]) {
+      if (this.chartLoading[queriedRange + queriedOffset]) {
         return;
       }
-      this.chartLoading[queriedRange] = true;
+      this.chartLoading[queriedRange + queriedOffset] = true;
       let chartData = null;
       try {
         const result = await this.$store.dispatch(
@@ -377,10 +432,10 @@ export default {
       } catch (e) {
         console.log(e);
       }
-      if (this.interactionsRange === queriedRange) {
+      if (this.interactionsRange === queriedRange && this.chartOffset === queriedOffset) {
         this.updateChart(chartData);
       }
-      this.chartLoading[queriedRange] = false;
+      this.chartLoading[queriedRange + queriedOffset] = false;
     },
     updateChart(chartData) {
       this.interactionsSeries[0].data.length = 0;
@@ -390,25 +445,72 @@ export default {
         return;
       }
 
+      let from = moment(chartData.from);
+      let to = moment(chartData.to);
+
       let dateFormat = "";
-      if (chartData.mode === "hour") {
-        dateFormat = "HH:00";
-      } else if (chartData.mode === "day") {
-        dateFormat = "MMM DD";
+      if (chartData.truncateTo === "hours") {
+        dateFormat = "[\u00A0]HH:00[\u00A0]";
+      } else if (chartData.truncateTo === "days") {
+        dateFormat = to.diff(from, "days") > 8 ? "DD" : "dddd";
       } else {
-        dateFormat = "MMM YYYY";
+        dateFormat = "MMM";
       }
 
-      let keys = Object.keys(chartData.interactions).sort();
-      for (let key of keys) {
-        let date = moment(key).format(dateFormat);
-        let interactionsPerDate = chartData.interactions[key];
+      let samples = {};
+      for (let key in chartData.interactions) {
+        let formattedDate = moment(key).format(dateFormat);
+        samples[formattedDate] = chartData.interactions[key] ?? 0;
+      }
 
-        this.interactionsSeries[0].data.push(interactionsPerDate ?? 0);
-        this.interactionsOptions.xaxis.categories.push(date);
+      let date = from.clone();
+      while (date.diff(to) < 0) {
+        let formattedDate = moment(date).format(dateFormat);
+        this.interactionsSeries[0].data.push(samples[formattedDate] ?? 0);
+        this.interactionsOptions.xaxis.categories.push(formattedDate);
+        date.add(1, chartData.truncateTo);
       }
     },
-  },
+    nth(d) {
+      if (d > 3 && d < 21) return d + "th";
+      switch (d % 10) {
+        case 1:
+          return d + "st";
+        case 2:
+          return d + "nd";
+        case 3:
+          return d + "rd";
+        default:
+          return d + "th";
+      }
+    },
+    weekOfMonth(input) {
+      const firstDayOfMonth = input.clone().startOf("month");
+      const firstDayOfWeek = firstDayOfMonth.clone().startOf("week");
+      const offset = firstDayOfMonth.diff(firstDayOfWeek, "days");
+      return Math.ceil((input.date() + offset) / 7);
+    },
+    jumpBy(offset) {
+      if (offset > 0 && !this.hasPrevRange) {
+        return;
+      }
+      this.chartOffset = Math.max(0, this.chartOffset + offset);
+    },
+    tooltipXFormatter(label) {
+      switch (this.interactionsRange) {
+        case "day":
+          return label + " - " + this.currentRangeStart.format("MMMM Do, YYYY");
+        case "week":
+          return label + " - " + this.currentRangeStart.format("MMMM, YYYY");
+        case "month":
+          return this.currentRangeStart.format("MMMM [" + this.nth(parseInt(label)) + "], YYYY");
+      }
+      return label;
+    },
+    tooltipYFormatter(value) {
+      return value.toFixed(0);
+    },
+},
 };
 </script>
 
